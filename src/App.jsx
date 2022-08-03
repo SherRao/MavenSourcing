02e58268bc77779ec3ae9bf7d6e6107d967af00a
Nausher Rao <SherRao15@gmail.com>
2022-08-03 07:27:29 -0400

    Start to Mantine-ify code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,78 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AppContainer, PageContainer, Navbar, Hero } from "@components";
+import { AppShell, Header, MantineProvider, SimpleGrid, Center, Title, Text } from "@mantine/core";
+import { Logo } from "@assets";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <AppContainer>
-            <Navbar />
-            <PageContainer>
-                <Hero />
-            </PageContainer>
-        </AppContainer>
+        <MantineProvider withNormalizeCSS>
+            <AppShell
+                padding="md"
+                fixed={false} //controls stickiness of header
+                header={
+                    <Header p="xl" color="red">
+                        <SimpleGrid cols={2} align="center" justify="center">
+                            <Center><img src={Logo} width="25%"/></Center>
+                            <Center><img src={Logo} width="25%"/></Center>
+                        </SimpleGrid>
+                    </Header>
+                }
+            >
+                <Title order={1}>Malleable, Valiant, Noble</Title>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+                <Text>A global sourcing company</Text>
+
+            </AppShell>
+        </MantineProvider>
     </React.StrictMode>
 );
